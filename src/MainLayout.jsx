1da2a0d5d2b846adc65d5d43e4f10caff53d29f2
hammadalla <hammadalla71@gmail.com>
2025-05-03 19:27:44 +0300

    fixpath

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from './Component/Navbar/Navbar'
+import Navbar from './Component/NavBar/Navbar'
 import WhatsAppButton from './Component/whatsappButton/WhatsappButton'
 import Footer from './Component/Footer/Footer'
 
